464d1d236f3bf42cdb46ad0756e6bbf449298454
jackR01 <133818752+jackR01@users.noreply.github.com>
2024-05-22 00:46:03 -0400

    carrusel(falta por terminar)- css card eliminado

--- scripts.js
+++ scripts.js
@@ -7,3 +7,20 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+//carrusel 
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+    //click en puntos
+    punto.forEach((cadapunto,i) => {
+        punto[i].addEventListener('click',()=>{
+            let posicion = i
+            let operacion = posicion * 50
+
+            grande.style.transform = `translateX(${operacion}%)`
+            punto.forEach( (cadapunto,i) =>{
+                punto[i].classList.remove('activo')
+            })
+            punto[i].classList.add('activo')
+        })
+    })
