ef43fd49c262c5135ca0d834b33f8a915756513e
jackR01 <133818752+jackR01@users.noreply.github.com>
2024-05-23 00:10:27 -0400

    agregados login y registro

--- scripts.js
+++ scripts.js
@@ -24,3 +24,17 @@ const punto = document.querySelectorAll('.punto')
             punto[i].classList.add('activo')
         })
     })
+
+//acordeon 
+const bloque = document.querySelectorAll('.bloque')
+const h2 = document.querySelectorAll('.h2')
+
+h2.forEach(( cadaH2 ,i ) => {
+    h2[i].addEventListener('click', () => {
+
+        bloque.forEach( ( cadaBloque , i) =>{
+            bloque[i].classList.remove('activo')
+        })
+        bloque[i].classList.add('activo')
+    })
+})
